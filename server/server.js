aefc3e3813ce6e7b9f8eda98f6ce37bdab27f7f1
ffeghali <fayfeghali@protonmail.com>
2023-04-09 21:42:56 -0400

    connected app to db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const recipeRoutes = require('./routes/recipeRoutes')
 
 // Express app
@@ -16,7 +17,14 @@ app.use((req, res, next) => {
 // Routes - (reacts to requests)
 app.use('/api/recipes', recipeRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("listening on port", process.env.PORT)
-})
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests after connect to db
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db and listening on port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
